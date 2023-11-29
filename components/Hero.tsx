12122153b50746990ae2e041b241fb45ed3778cb
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-11-29 23:53:41 +0100

    Two Buttons added for Download App and How we work

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Button from "./Button"
 
 const Hero = () => {
   return (
@@ -39,9 +40,22 @@ const Hero = () => {
           <p className="bold-16 lg:bold-20 text-blue-70">
             198k
             <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
-
           </p>
         </div>
+         <div className="flex w-full gap-3 sm:flex-row">
+              <Button 
+              type="button" 
+              title="Download App"
+              variant="btn_green" 
+              />
+               <Button 
+              type="button" 
+              title="How we work"
+              icon="/play.svg"
+              variant="btn_white_text" 
+              />
+
+         </div>
     </div>
     </section>
   )
