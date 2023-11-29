a247dce1c265961e0d76608dd613fbd1b95b8851
Oluwatobi Adegelu <adeniyi.adegelu@gmail.com>
2023-11-29 23:17:06 +0100

    Review on Hero Section done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,6 +19,29 @@ const Hero = () => {
        w-10 lg:w-[50px]"
        />
        <h1 className="bold-52 lg:bold-88">Grand LakeView Camp Area</h1>
+       <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+        We want to be on each of your journeys seeking the 
+        satisfaction of seeing the incprruptible beauty of nature. We 
+        can help you on an adventure around the world in just one app. 
+        </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5).fill(1).map((_, index) =>(
+              <Image 
+              src="/star.svg"
+              key={index}
+              alt="star"
+              width={24}
+              height={24} 
+              />
+            ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
+
+          </p>
+        </div>
     </div>
     </section>
   )
